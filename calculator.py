36c233f485371384d9126c742f88e2bc25f7f8f7
Furrettophat <infintecarnation@gmail.com>
2025-11-16 18:46:36 -0500

    comit 1 lol

--- calculator.py
+++ calculator.py
@@ -4,8 +4,29 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
+# https://docs.google.com/document/d/17mvLUx1jLvktsB2fbov1l4P5_C3fEM9ayaUcBbuAtLc/edit?tab=t.0
+import math
+
 def add(a, b): 
-    pass
+    return a + b
+
+def subtract(a, b): 
+    return a - b
+
+def multiply(a, b): 
+    return a * b
+
+def divide(a, b): 
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or b <= 0:
+        raise ValueError
+    math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
 
 
