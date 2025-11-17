4770e1e8297b02b6514f6de86564eace63475a69
Furrettophat <infintecarnation@gmail.com>
2025-11-16 19:31:45 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,21 +2,24 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
+    ######## Partner 2
     # def test_add(self): # 3 assertions
     #     fill in code
 
     # def test_subtract(self): # 3 assertions
     #     fill in code
-    # ##########################
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    ####### Partner 1
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(6, 4), 24)
+        self.assertEqual(mul(-2, -9), 18)
+        self.assertEqual(mul(-3, 5), -15)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(2, 8), 4)
+        self.assertEqual(div(5, -45), -9)
+        self.assertAlmostEqual(div(3, 1), (1/3))
+    ##########################
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,21 +37,22 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        # call log function inside, example:
+        with self.assertRaises(ValueError):
+            logarithm(0, 0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(7, -24), 25)
+        self.assertEqual(hypotenuse(-8, -15), 17)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+            square_root(-4)
+            square_root(-16)
+        self.assertEqual(square_root(9), 3)
+        self.assertEqual(square_root(25), 5)
     ##########################
 
 # Do not touch this
